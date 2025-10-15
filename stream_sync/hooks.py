800cc17254a998ad7fa2f9d576231fcec2da8ce3
krisnajufer <krisnajufer07@gmail.com>
2025-10-15 14:51:13 +0700

    UPDATE: ignore_permissions and customer autoname

--- stream_sync/hooks.py
+++ stream_sync/hooks.py
@@ -129,9 +129,9 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Customer": "stream_sync.custom.customer.CustomerCustom"
+}
 
 # Document Events
 # ---------------
